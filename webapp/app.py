b4bdf0bea3e444fcaf3e6016a4f9ccb65fbfbca7
wmsj100 <wmsj100@hotmail.com>
2020-07-09 18:15:57 +0800

    nginx flask mysql container up ok

---
+++ webapp/app.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020  <@57ecb7a85357>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+from flask import Flask, Response, render_template_string
+import pymysql
+import json
+
+app=Flask(__name__)
+
+class DbOperate(object):
+    def __init__(self):
+        db = pymysql.connect('mysql', 'root', '123456', 'webapp')
+
+@app.route('/')
+def index():
+	return "Hello world\n"
+
+@app.route('/api')
+def api():
+    try:
+        db = DbOperate()
+    except:
+        return render_template_string('<h1>Mysql Can not connect</h1>')
+    return render_template_string('<h1>Hello Flask !!!</h1>')
+	#return Response(json.dumps(user), mimetype='application/json')
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0')
