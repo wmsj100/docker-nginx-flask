a065b73727e8dfa22523c0a5bdbf3c0846f4b263
wmsj100 <wmsj100@hotmail.com>
2020-07-09 16:17:45 +0800

    add flask

---
+++ flask/run_supervisord.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#
+# run.sh
+# Copyright (C) 2020 ubuntu <ubuntu@txOS>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+/usr/local/bin/supervisord -c /etc/supervisord.d/supervisord.conf
